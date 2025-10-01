e081e1491de0e23ee8012a4478ddff74b79ec5c4
Lucas Menezes <lucasfolha418@gmail.com>
2025-09-30 23:27:48 -0300

    Create Pessoa.java
    add class Pessoa

---
+++ src/entidades/Pessoa.java
@@ -0,0 +1,37 @@
+package entidades;
+
+public abstract class Pessoa{
+    private String nome;
+    private String cpf;
+    private int idade;
+
+    public Pessoa(String nome, String cpf, int idade){
+        this.nome = nome;
+        this.cpf = cpf;
+        this.idade = idade;
+    }
+
+    public String getNome(){
+        return nome;
+    }
+
+     public String getCpf(){
+        return cpf;
+    }
+
+     public int getIdade(){
+        return idade;
+    }
+
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+
+    public void setCpf(String cpf){
+        this.cpf = cpf;
+    }
+
+    public void setIdade(int idade){
+        this.idade = idade;
+    }
+}
