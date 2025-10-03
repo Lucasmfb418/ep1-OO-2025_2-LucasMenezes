2f62b03ef58c4af8ca98fbd1bd19299d55c010a2
Lucas Menezes <lucasfolha418@gmail.com>
2025-10-03 09:42:19 -0300

    Create Medico.java
    add class medicos

---
+++ src/entidades/Medico.java
@@ -0,0 +1,40 @@
+package entidades;
+
+import java.util.ArrayList;
+
+import javax.print.DocFlavor.STRING;
+
+public class Medico extends Pessoa {
+    private String crm;
+    private String especialidade;
+    private double custo;
+    private ArrayList<Consulta> agenda;
+
+    public Medico(String nome, String crm, String especialidade, double custo){
+        super(nome, null, 0);
+        this.crm = crm;
+        this.especialidade = especialidade;
+        this.custo = custo;
+        this.agenda = new ArrayList<>();
+    }
+
+    public String getCrm(){
+        return crm;
+    }
+
+    public String getEspecialidade(){
+        return especialidade;
+    }
+
+    public double getCusto(){
+        return custo;
+    }
+
+    public ArrayList<Consulta> getAgenda(){
+        return agenda;
+    }
+
+    public void adicionarConsulta(Consulta c){
+        agenda.add(c);
+    }
+}
