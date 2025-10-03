0caaa14d523757860c901114a3d299b8e4dd24de
Lucas Menezes <lucasfolha418@gmail.com>
2025-10-03 09:43:34 -0300

    Create PlanoSaude.java
    add class plano

---
+++ src/entidades/PlanoSaude.java
@@ -0,0 +1,30 @@
+package entidades;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class PlanoSaude{
+    private String nome;
+    private HashMap<String, Double> descontos;
+
+    public PlanoSaude(String nome){
+        this.nome = nome;
+        this.descontos = new HashMap<>();
+    }
+
+    public String getNome(){
+        return nome;
+    }
+
+    public void adicionarDesconto(String especialidade, double desconto){
+        descontos.put(especialidade, desconto);
+    }
+
+    public double getDesconto(String especialidade){
+        return descontos.getOrDefault(especialidade, 0.0);
+    }
+
+    public Map<Object, Object> getDescontos(){
+        return Map.of();
+    }
+}
